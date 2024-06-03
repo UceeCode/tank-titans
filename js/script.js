082c3e63acc66fac927c916c8dfb7d493deba451
Frank.Lin <writefranklyn@gmail.com>
2024-06-03 23:52:13 +0100

    fix: fixed errors && made the shotter tank rotate properly

--- js/script.js
+++ js/script.js
@@ -1,47 +1,51 @@
-let btnStart = document.querySelector('.btnStart');
-let gameOverElement = document.getElementById('gameOverElement');
-let container = document.querySelector('.container');
-let box = document.querySelector('.box');
-let boxCenter = [box.offsetLeft + (box.offsetWidth / 2), box.offsetTop + (box.offsetHeight / 2)];
+const btnStart = document.querySelector('.btnStart');
+const gameOverEle = document.getElementById('gameOverEle');
+const container = document.getElementById('container');
+const box = document.querySelector('.box');
+const boxCenter = [box.offsetLeft + (box.offsetWidth / 2)
+                  , box.offsetTop + (box.offsetHeight / 2)];
 console.log(boxCenter);
+let gamePlay = false;
 let player;
 let animateGame;
-
-let gamePlay = false;
-
 btnStart.addEventListener('click', startGame);
-container.addEventListener('click', mouseClicked);
-container.addEventListener('mousemove', movePosition);
+container.addEventListener('mousedown', mouseDown);
+container.addEventListener('mousemove', movePostion);
 
-function movePosition(e){
-    let mouseAngle = getDeg(e);
-    console.log(mouseAngle);
+function movePostion(e) {
 
+    let deg = getDeg(e);
+    box.style.webkitTransform = 'rotate(' + deg + 'deg)';
+    box.style.mozTransform = 'rotate(' + deg + 'deg)';
+    box.style.msTransform = 'rotate(' + deg + 'deg)';
+    box.style.oTransform = 'rotate(' + deg + 'deg)';
+    box.style.transform = 'rotate(' + deg + 'deg)';
 }
 
-function getDeg(e){
-    let angle = Math.atan2(e.clientX-boxCenter[0], -(e.clientY - boxCenter[1]));
+function getDeg(e) {
+    let angle = Math.atan2(e.clientX - boxCenter[0], -(e.clientY - boxCenter[1]));
     return angle * (180 / Math.PI);
 }
 
-function mouseClicked(e){
-    if(gamePlay == true){
+function mouseDown(e) {
+    if (gamePlay) {
+        console.log('FIRE');
+    }
+}
+
+function startGame() {
+    gamePlay = true;
+    gameOverEle.style.display = 'none';
+    player = {
+            score: 0
+            , barwidth: 100
+            , lives: 100
+        }
+    animateGame = requestAnimationFrame(playGame);
+}
+
+function playGame() {
+    if (gamePlay) {
         animateGame = requestAnimationFrame(playGame);
     }
-}
-
-function startGame(){
-    gamePlay = true;
-    gameOverElement.style.display = 'none';
-    player = {
-        score: 0,
-        barwidth: 100,
-        lives: 100
-    }
-    animateGame = requestAnimationFrame(playGame);
-}
-
-function playGame(){
-    console.log('in play');
-    animateGame = requestAnimationFrame(playGame);
 }
