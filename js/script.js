48e65789c92a55e739cea7d730ab00937e6b756b
Frank.Lin <writefranklyn@gmail.com>
2024-06-03 23:30:22 +0100

    feat: implemented a function to get the degree of the position the mouse is moving at

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let btnStart = document.querySelector('.btnStart');
 let gameOverElement = document.getElementById('gameOverElement');
 let container = document.getElementById('container');
+let box = document.querySelector('.box');
+let boxCenter = [box.offsetLeft + (box.offsetRight / 2), box.offsetTop + (box.offsetHeight / 2)];
+console.log(boxCenter);
 let player;
 let animateGame;
 
@@ -8,6 +11,19 @@ let gamePlay = false;
 
 btnStart.addEventListener('click', startGame);
 container.addEventListener('click', mouseClicked);
+container.addEventListener('mousemove', movePosition);
+
+function movePosition(e){
+    console.log(e);
+    let mouseAngle = getDeg(e);
+    console.log(mouseAngle);
+
+}
+
+function getDeg(e){
+    let angle = Math.atan2(e.clientX-boxCenter[0], -(e.clientY - boxCenter[1]));
+    return angle * (180 / Math.PI);
+}
 
 function mouseClicked(e){
     if(gamePlay = true){
