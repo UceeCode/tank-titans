5cc8ad7a6407f5a753697154a4bc4c1488755fc1
Frank.Lin <writefranklyn@gmail.com>
2024-06-03 22:52:52 +0100

    feat: initialized a click event function to fire shots when the mouse is clicked

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 let btnStart = document.querySelector('.btnStart');
 let gameOverElement = document.getElementById('gameOverElement');
+let container = document.getElementById('container');
 let player;
 let animateGame;
 
 let gamePlay = false;
 
 btnStart.addEventListener('click', startGame);
+container.addEventListener('click', mouseClicked);
+
+function mouseClicked(e){
+    if(gamePlay = true){
+        animateGame = requestAnimationFrame(playGame);
+    }
+}
 
 function startGame(){
     gamePlay = true;
